ae2683bfa33b7cabe23bddd988aa72722842909c
Laparevich Daniil <danik.laparevich@bk.ru>
2023-09-20 18:03:34 +0300

    fix: annotation in UserController

--- src/main/java/com/example/springfirstrestapi/api/controller/UserController.java
+++ src/main/java/com/example/springfirstrestapi/api/controller/UserController.java
@@ -5,9 +5,11 @@ import com.example.springfirstrestapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@RestController
 public class UserController {
 
     private UserService userService;
