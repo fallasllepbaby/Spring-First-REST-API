b4fe0c8c03cea101905fabc313f9fa19c37309df
Laparevich Daniil <danik.laparevich@bk.ru>
2023-09-20 17:52:23 +0300

    add: UserController

--- src/main/java/com/example/springfirstrestapi/api/controller/UserController.java
+++ src/main/java/com/example/springfirstrestapi/api/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.example.springfirstrestapi.api.controller;
 
+import com.example.springfirstrestapi.api.model.User;
+import com.example.springfirstrestapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public User getUser(@RequestParam Integer id) {
+        return userService.getUser(id);
+    }
 }
--- src/main/java/com/example/springfirstrestapi/service/UserService.java
+++ src/main/java/com/example/springfirstrestapi/service/UserService.java
@@ -1,4 +1,20 @@
 package com.example.springfirstrestapi.service;
 
+import com.example.springfirstrestapi.api.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+
+    private List<User> userList;
+
+    public UserService() {
+        userList = new ArrayList<>();
+
+        User user1 = new User(1, "Ida", 32, "ida@mail.com");
+        User user2 = new User(2,"Hans")
+    }
+    public User getUser(Integer id) {
+    }
 }
